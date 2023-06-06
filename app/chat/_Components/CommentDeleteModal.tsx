019ebda39c99ab842a014972da592f128e02d29c
kennystwart <kennystwork@gmail.com>
2023-06-06 12:30:28 -0400

    final chat page

---
+++ app/chat/_Components/CommentDeleteModal.tsx
@@ -0,0 +1,25 @@
+import Modal from '@/components/Modal';
+import React from 'react';
+
+interface Props {
+    show: boolean,
+    setShow: (boolean) => void,
+    message: string,
+    removeMessage: (messageId:string) => void
+}
+
+const CommentDeleteModal: React.FC<Props> = ({show, setShow, message, removeMessage}) => {
+
+    const submit = () => {
+        removeMessage(message);
+    };
+    
+    return (
+        <>
+            <Modal title='Danger Alert!' show={show} setShow={setShow} type="warning" submit={submit}> 
+                <p>Remove this message?</p>
+            </Modal> 
+        </>
+    );
+}
+export default CommentDeleteModal;
